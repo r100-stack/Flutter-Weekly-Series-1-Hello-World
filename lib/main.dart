236b1a648aea19a1d3e0abc815a042c370adb21e
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-02 14:48:17 -0500

    Added TODOs for Step 4

--- lib/main.dart
+++ lib/main.dart
@@ -4,44 +4,25 @@ void main() {
   runApp(MyApp());
 }
 
-// TODO (1): Replace Container() with MaterialApp()
-// We do this as MyApp() should build us a MaterialApp and not just any widget.
-// MaterialApps are great encapsulators for all our app's widgets, functionality and more.
-// TODO (2): Assign the "home" field with a Text widget.
-// The home field is the home page/widget of the app.
-// TODO (3): The Text widget can contain any text you want. Eg. 'Hello World'.
-// Notice how the text appears red with two underlines. This is not how real apps look!
-// Hence, to make it look like a real app, we use an app definition widget called Scaffold.
-// TODO (4): Move your cursor to (Click on) the Text Widget.
-// TODO (5): Press Alt+Enter (Ctrl+. for VS Code) to bring up the intention actions.
-// TODO (6): Choose "Wrap with widget"
-// TODO (7): Next, type Scaffold as the widget.
-// Notice how we get an error. This is because Scaffold doesn't have a property called child.
-// Hence, let's assign our Text widget to a property that the Scaffold has — body.
-// TODO (8): Change "child" to "body"
-// Observe how we now get a white screen and the text looks elegant yet simple.
-
-// TODO (9): Press Ctrl+Alt+L (Shift+Alt+F) to reformat the code.
-// In Flutter, to get proper reformatting, use commas(,).
-// For example,
-//
-// return MaterialApp(home: Scaffold(body: Text('Hello World')),);
-// reformats to,
-// return MaterialApp(
-//      home: Scaffold(body: Text('Hello World')),
-//    );
-//
-// whereas,
-// return MaterialApp(home: Scaffold(body: Text('Hello World'),),);
-// reformats to,
-// return MaterialApp(
-//   home: Scaffold(
-//     body: Text('Hello World'),
-//   ),
-// );
-//
-// This is because of the extra comma after the text widget.
-// Don't worry too much about this. This will be clear with time!
+// Apps usually have bar at the top which contains a title along with some icons.
+// The icon on the left could be a back button
+// The icon on right could be an icon for user information or a shopping cart.
+// Let's implement a simple AppBar with just a title for now.
+// TODO (1): Under the Scaffold widget, assign the appBar property with AppBar().
+// The AppBar widget has a property called title.
+// Title takes in a Text widget. The same Text widget we used before!
+// Let's add a "Hello World" title to the AppBar
+// TODO (2): Under the AppBar widget, assign the title property with a Text() widget.
+// TODO (3): Have the Text() widget to show "Hello World!" or something similar.
+// We can also change the color of the AppBar.
+// Google Material Design specifies a palette of standard colors. More info: https://material.io/design/color/the-color-system.html#tools-for-picking-colors
+// We can easily access these colors using the Colors class.
+// For example, Colors.red gives and Colors.lightBlue use the red and light blue colors respectively.
+// We can also get shared of a particular color
+// For example, Colors.red[900] and Colors.red[100] are a darker and lighter shades of red.
+// Shade 500 is the default shade for material colors.
+// Let's add a preferred color to our AppBar using the backgroundColor property.
+// TODO (4): Assign the backgroundColor property of the AppBar to your preferred color (using the Colors class).
 
 // NOTE: If you get any type of errors, try doing a cold restart.
 // A cold restart involves pressing the stop button and then pressing the play button again.
