794391c23b27ec3937a29784bc03568f6dd52d1a
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-02 10:42:10 -0500

    Added TODOs for step 2

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,11 @@ void main() {
 }
 
 // TODO (1): Type "stl". This is the shortcut to create a stateless widget
+// Everything in Flutter are composed widgets. Even apps are widgets! Hence, we
+// are creating this stateless widget to hold the contents of our app.
 // TODO (2): Name the stateless widget, "MyApp"
-// TODO (3): Now replace the null on line 4 with MyApp(). This is because we want
-//  to run our MyApp() widget when the app starts.
+// TODO (3): Now replace the null on line 4 with MyApp().
+// Doing this runs our newly created widget that holds the contents of our app
+
+// NOTE: Running the app now may give errors. This is normal! It will be fixed
+// in the coming steps.
