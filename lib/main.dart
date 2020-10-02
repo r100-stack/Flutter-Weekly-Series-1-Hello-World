261b814d86e5a15000972be7046859abebf94ff5
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-02 15:09:27 -0500

    Added TODOs for Step 5

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,30 @@ void main() {
   runApp(MyApp());
 }
 
-// Apps usually have bar at the top which contains a title along with some icons.
-// The icon on the left could be a back button
-// The icon on right could be an icon for user information or a shopping cart.
-// Let's implement a simple AppBar with just a title for now.
-// TODO (1): Under the Scaffold widget, assign the appBar property with AppBar().
-// The AppBar widget has a property called title.
-// Title takes in a Text widget. The same Text widget we used before!
-// Let's add a "Hello World" title to the AppBar
-// TODO (2): Under the AppBar widget, assign the title property with a Text() widget.
-// TODO (3): Have the Text() widget to show "Hello World!" or something similar.
-// We can also change the color of the AppBar.
-// Google Material Design specifies a palette of standard colors. More info: https://material.io/design/color/the-color-system.html#tools-for-picking-colors
-// We can easily access these colors using the Colors class.
-// For example, Colors.red gives and Colors.lightBlue use the red and light blue colors respectively.
-// We can also get shared of a particular color
-// For example, Colors.red[900] and Colors.red[100] are a darker and lighter shades of red.
-// Shade 500 is the default shade for material colors.
-// Let's add a preferred color to our AppBar using the backgroundColor property.
-// TODO (4): Assign the backgroundColor property of the AppBar to your preferred color (using the Colors class).
-
-// NOTE: If you get any type of errors, try doing a cold restart.
-// A cold restart involves pressing the stop button and then pressing the play button again.
+// Notice how the Hello World Text is sticking to the top left.
+// We want to center this text on the screen.
+// To do this, we can use a helper widget called Center()
+// Center takes a widget for its child peoperty. The child is then centered in the parent of Center.
+// Hence, our Text() widget will be centered in the screen as the parent of the
+// Center() widget is the screen itself.
+// TODO (1): Move the cursor to (click on) the Text() widget passed as a parameter for the body field of the Scaffold.
+// TODO (2): Hit Alt+Enter (Ctrl+. for VS Code) to show the intention actions.
+// TODO (3): Select "Wrap with Center".
+// Now let's style our text.
+// To style our Text, we use the TextStyle object.
+// This TextStyle object can be passed to the style property of the Text().
+// TODO (4): Assign a TextStyle() object to the style property of the text widget. (the one in the scaffold, and not the appbar)
+// TextStyle can take in a bunch of styling parameters.
+// The ones that we'll focus on today are fontSize, color, and fontWeight.
+// fontSize is the size of the text in logical pixels.
+// color is the color of the text.
+// fontWeight is the thickness of the text. Eg. bold or normal.
+// fontWeight in Flutter is defined by the FontWeight class.
+// For instance, FontWeight.w100 and FontWeight.w900 are thin and think font weights respectively.
+// FontWeight also has a FontWeight.normal and FontWeight.bold value as aliases for w400 and w700.
+// TODO (5): Add a suitable size (as a double) to the fontSize property of the TextStyle. eg. 40.0
+// TODO (6): Add a suitable color (using Colors class) to the color property of the TextStyle.
+// TODO (7): Add a suitable font weight (using FontWeight class) to the fontWeight property of the TextStyle.
 
 class MyApp extends StatelessWidget {
   @override
