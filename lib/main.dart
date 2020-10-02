b8a1a35d5db720f33578ed9cbfb9d5861c54bcff
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-02 10:17:53 -0500

    Added TODOs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(null); // The parameter we pass here is the App we want to run. We will do that in the next step
+  runApp(null);
 }
+
+// TODO (1): Type "stl". This is the shortcut to create a stateless widget
+// TODO (2): Name the stateless widget, "MyApp"
+// TODO (3): Now replace the null on line 4 with MyApp(). This is because we want
+//  to run our MyApp() widget when the app starts.
