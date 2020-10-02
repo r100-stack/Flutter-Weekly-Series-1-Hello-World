214a3cf80aa2eb677113035e85fbf1de19ae3fc3
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-02 10:38:16 -0500

    Added TODOs for Step1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(null); // The parameter we pass here is the App we want to run. We will do that in the next step
-}
+// TODO (1): Write a main method of type void
+// TODO (2): Within main(), call the runApp() method.
+// TODO (3): Notice how you get an error? This is because runApp is not defined. We need to import a library that defines runApp.
+// TODO (4): Move the cursor to the squiggly line under runApp.
+// TODO (5): Tap Alt+Enter (Ctrl+. in VS Code).
+// TODO (6): Notice how you have three libraries to choose from
+//  1. 'package:flutter/material.dart': Material package contains all the widgets,
+//      methods, etc. according to the material design guidelines.
+//  2. 'package:flutter/cupertino.dart': Cupertino package contains all the widgets,
+//      methods, etc. according to the iOS design guidelines.
+//  3. 'package:flutter/widgets.dart': The widgets package is a subset of the material
+//      package. Hence, we can import the parent materials package instead.
+// We almost always choose the material package as we prefer to follow Google's
+// cross-platform Material Design Guidelines
+// TODO (7): Choose the material package from the Alt+Enter intention actions.
+// TODO (8): Pass null as a parameter to runApp(). We will implement the app to run in the next steps.
